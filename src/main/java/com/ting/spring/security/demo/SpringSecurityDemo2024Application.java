21bced7bdbcdc6436ffe7c0430a299923034a299
DESKTOP-E0AM5JD\ting <wisezt@hotmail.com>
2024-02-01 14:40:08 +0800

    Initializing...

---
+++ src/main/java/com/ting/spring/security/demo/SpringSecurityDemo2024Application.java
@@ -0,0 +1,13 @@
+package com.ting.spring.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemo2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemo2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ting/spring/security/demo/SpringSecurityDemo2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ting.spring.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemo2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
