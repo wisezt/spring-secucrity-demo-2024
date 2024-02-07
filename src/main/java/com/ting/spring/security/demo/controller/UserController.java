f010bf10f303d6c9be763d398b8e11ecdc702fbd
DESKTOP-E0AM5JD\ting <wisezt@hotmail.com>
2024-02-07 13:13:27 +0800

    Username/Password Authentication in memory

--- src/main/java/com/ting/spring/security/demo/controller/UserController.java
+++ src/main/java/com/ting/spring/security/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ting.spring.security.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,19 @@ public class UserController {
 
     @GetMapping("/")
     public String getHome(){
-        return "Home: Hello World!";
+        return "Home: Hello World! v01";
     }
 
 
     @GetMapping("/test")
     public String getTest(){
         return "Test: Hello World!";
+    }
+
+    @PostMapping("/my/logout")
+    public String performLogout() {
+        // .. perform logout
+        return "redirect:/home";
     }
 
 
